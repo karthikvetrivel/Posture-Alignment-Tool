9a420b43c526a43255914f457321d3b6d1b4f4ef
karthikvetrivel <kvvetrivel@gmail.com>
2020-10-17 20:49:15 -0700

    added UI changes

--- PostNet Congressional App Challenge/mainPosture.js
+++ PostNet Congressional App Challenge/mainPosture.js
@@ -8,7 +8,10 @@ let skeleton;
 let ellipseSize = 16;
 
 function setup() {
-  createCanvas(640, 480);
+  let cnv = createCanvas(640, 480);
+  background(0, 0, 0);
+  cnv.parent("videoframe");
+
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
